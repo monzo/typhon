929d80f3088e760ef2c81d34f68da5cfa8dc7998
Vincenzo Prignano <vincenzo.prignano@gmail.com>
2015-02-12 18:51:09 +0000

    Scratch server

--- server/server.go
+++ server/server.go
@@ -1,7 +1,26 @@
 package server
 
-import "github.com/vinceprignano/bunny/server/endpoint"
+import (
+	"os"
+	"time"
+
+	log "github.com/cihub/seelog"
+	"github.com/vinceprignano/bunny/server/endpoint"
+	"github.com/vinceprignano/bunny/transport"
+)
 
 type Server struct {
+	Name      string
 	endpoints map[string]endpoint.Endpoint
+	Transport transport.Transport
+}
+
+func (s *Server) Init() {
+	select {
+	case s.Transport = <-s.Transport.Connect():
+		log.Info("[Server] Connected to transport")
+	case <-time.After(10 * time.Second):
+		log.Critical("[Server] Failed to connect to transport")
+		os.Exit(1)
+	}
 }
