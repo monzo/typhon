9104ba1d7bcea22038725ee372ea2b80aef043f9
Matt Heath <matt@mattheath.com>
2015-02-23 16:01:32 +0000

    Move default client across into client.go

--- client/client.go
+++ client/client.go
@@ -6,3 +6,9 @@ type Client interface {
 	Init()
 	Call(serviceName, endpoint string, req proto.Message, res proto.Message) error
 }
+
+var DefaultClient Client = NewRabbitClient()
+
+func Request(serviceName, endpoint string, req proto.Message, res proto.Message) error {
+	return DefaultClient.Call(serviceName, endpoint, req, res)
+}
