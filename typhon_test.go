cd365d4aaf679c2511ae95a119b1d2fc5632c87c
Jonas Huckestein <jonas.huckestein@gmail.com>
2015-02-24 22:55:18 +0000

    fix typhon_test.go

--- typhon_test.go
+++ typhon_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/b2aio/typhon/client"
 	"github.com/b2aio/typhon/example/proto/callhello"
 	"github.com/b2aio/typhon/server"
-	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/require"
 
 	"github.com/b2aio/typhon/example/handler"
@@ -27,7 +26,7 @@ func initServer(t *testing.T) {
 	go server.Run()
 
 	select {
-	case <-server.DefaultServer.NotifyConnected():
+	case <-server.NotifyConnected():
 	case <-time.After(1 * time.Second):
 		t.Fatalf("StubServer couldn't connect to RabbitMQ")
 	}
@@ -42,11 +41,11 @@ func TestExample(t *testing.T) {
 	resp := &callhello.Response{}
 	client.Request(
 		"example.callhello",
-		&callhello.Request{Value: proto.String("Bunny")},
+		&callhello.Request{Value: "Bunny"},
 		resp,
 	)
-
-	require.Equal(t, "example.hello says 'Hello, Bunny!'", resp.GetValue())
-	// Log the response we receive
 	t.Logf("[testHandler] received %s", resp)
+	require.Equal(t, "example.hello says 'Hello, Bunny!'", resp.Value)
+	// Log the response we receive
+
 }
