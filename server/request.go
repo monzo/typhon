db014dd6098d2db31ab62dd37ceb68b528f2d2ca
Matt Heath <matt@mattheath.com>
2015-02-25 14:39:47 +0000

    Add scopedRequest to the server.Request interface

--- server/request.go
+++ server/request.go
@@ -1,6 +1,9 @@
 package server
 
-import "golang.org/x/net/context"
+import (
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/context"
+)
 
 type Request interface {
 	context.Context
@@ -16,4 +19,8 @@ type Request interface {
 	// Details about the service and endpoint being called
 	Service() string
 	Endpoint() string
+
+	// ScopedRequest makes a client request within the scope of the current request
+	// @todo change the request & response interface to decouple from protobuf
+	ScopedRequest(service string, endpoint string, req proto.Message, resp proto.Message) error
 }
