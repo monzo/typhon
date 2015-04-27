288fcef751e8af91fb9ce9cba37642c191338305
Jonas Huckestein <jonas.huckestein@gmail.com>
2015-04-27 23:12:34 +0100

    unmarshal nil maps as empty maps

--- errors/marshaling_test.go
+++ errors/marshaling_test.go
@@ -26,9 +26,9 @@ func TestUnmarshalNilError(t *testing.T) {
 	assert.Equal(t, "Nil error unmarshalled!", platError.Message)
 }
 
-// interchangingErrorTestCases represents a set of error formats
-// which should be converted between
-var interchangableErrorTestCases = []struct {
+// marshalTestCases represents a set of error formats
+// which should be marshaled
+var marshalTestCases = []struct {
 	platErr  *Error
 	protoErr *pe.Error
 }{
@@ -104,7 +104,7 @@ var interchangableErrorTestCases = []struct {
 }
 
 func TestMarshal(t *testing.T) {
-	for _, tc := range interchangableErrorTestCases {
+	for _, tc := range marshalTestCases {
 		protoError := Marshal(tc.platErr)
 		assert.Equal(t, tc.protoErr.Code, protoError.Code)
 		assert.Equal(t, tc.protoErr.Message, protoError.Message)
@@ -113,8 +113,90 @@ func TestMarshal(t *testing.T) {
 	}
 }
 
+// these are separate from above because the marshaling and unmarshaling isn'y symmetric.
+// protobuf turns empty maps[string]string into nil :(
+var unmarshalTestCases = []struct {
+	platErr  *Error
+	protoErr *pe.Error
+}{
+	{
+		&Error{
+			PublicContext:  map[string]string{},
+			PrivateContext: map[string]string{},
+		},
+		&pe.Error{},
+	},
+	{
+		&Error{
+			PublicContext:  map[string]string{},
+			PrivateContext: map[string]string{},
+		},
+		&pe.Error{
+			Code: ErrUnknown,
+		},
+	},
+	{
+		&Error{
+			Code:    ErrTimeout,
+			Message: "omg help plz",
+			PublicContext: map[string]string{
+				"something": "hullo",
+			},
+			PrivateContext: map[string]string{
+				"something else": "bye bye",
+			},
+			Stack: []stack.Frame{
+				{
+					Filename: "some file",
+					Line:     123,
+					Method:   "someMethod",
+				},
+				{
+					Filename: "another file",
+					Line:     1,
+					Method:   "someOtherMethod",
+				},
+			},
+		},
+		&pe.Error{
+			Code:    ErrTimeout,
+			Message: "omg help plz",
+			PublicContext: map[string]string{
+				"something": "hullo",
+			},
+			PrivateContext: map[string]string{
+				"something else": "bye bye",
+			},
+			Stack: []*pe.StackFrame{
+				{
+					Filename: "some file",
+					Line:     123,
+					Method:   "someMethod",
+				},
+				{
+					Filename: "another file",
+					Line:     1,
+					Method:   "someOtherMethod",
+				},
+			},
+		},
+	},
+	{
+		&Error{
+			Code:           ErrForbidden,
+			Message:        "NO. FORBIDDEN",
+			PublicContext:  map[string]string{},
+			PrivateContext: map[string]string{},
+		},
+		&pe.Error{
+			Code:    ErrForbidden,
+			Message: "NO. FORBIDDEN",
+		},
+	},
+}
+
 func TestUnmarshal(t *testing.T) {
-	for _, tc := range interchangableErrorTestCases {
+	for _, tc := range unmarshalTestCases {
 		platErr := Unmarshal(tc.protoErr)
 		assert.Equal(t, tc.platErr.Code, platErr.Code)
 		assert.Equal(t, tc.platErr.Message, platErr.Message)
