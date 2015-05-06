aa70d9276af7e3accff8f427b544ed6db674254b
Matt Heath <matt@mattheath.com>
2015-05-06 15:10:17 +0100

    Add authorizers with a 'None' example

---
+++ auth/authorizers.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	log "github.com/cihub/seelog"
+	"golang.org/x/net/context"
+)
+
+// DefaultAuthorizer is an authorizer used on endpoint registration if the endpoint
+// does not have an authorizer specified. It is STRONGLY recommended that this be replaced
+// by a significantly more secure authorizer, preventing unintended access to endpoints
+var DefaultAuthorizer = None()
+
+// None authorizer requires no authorization and is therefore accessible to all callers
+func None() func(ctx context.Context, creds Credentials) error {
+	return func(ctx context.Context, creds Credentials) error {
+		log.Debugf("No auth required for endpoint as the None authorizer is being used")
+		return nil
+	}
+}
