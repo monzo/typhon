91423b229344b67d0e30b03e148c2807baf4ef6a
Matt Heath <matt@mattheath.com>
2015-05-06 13:03:54 +0100

    Add User and Client to the credential interface

--- auth/auth.go
+++ auth/auth.go
@@ -18,9 +18,23 @@ type Credentials interface {
 	AccessToken() string
 	RefreshToken() string
 	Expiry() time.Time
-	Scopes() []string
+	Scopes() []string // aggregated scope information from a combination of the user and client scopes
+	User() User
+	Client() Client
 }
 
 // Authorizer provides an interface to validate authorization credentials
 // for access to resources, eg. oauth scopes, or other access control
 type Authorizer func(ctx context.Context, creds Credentials) error
+
+// User represents the resource owner ie. an end-user of the application
+type User interface {
+	ID() string
+	Scopes() []string
+}
+
+// Client represents the application making a request on behalf of a User
+type Client interface {
+	ID() string
+	Scopes() []string
+}
