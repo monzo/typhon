108aac853a023a07bbf8dc26a577e2019f8e3830
Oliver Beattie <oliver@obeattie.com>
2016-06-24 14:18:00 +0100

    Non-terrors to errors

--- terrors.go
+++ terrors.go
@@ -65,18 +65,21 @@ func ErrorFilter(req Request, svc Service) Response {
 		rsp.StatusCode = ErrorStatusCode(terr)
 		rsp.Header.Set("Terror", "1")
 	} else {
-		if rsp.StatusCode >= 400 && rsp.StatusCode <= 599 && rsp.Header.Get("Terror") == "1" {
+		if rsp.StatusCode >= 400 && rsp.StatusCode <= 599 {
 			b, _ := rsp.BodyBytes(false)
-			tp := &terrorsproto.Error{}
-			if err := json.Unmarshal(b, tp); err != nil {
-				log.Warn(rsp.ctx, "Failed to unmarshal terror: %v", err)
+			switch rsp.Header.Get("Terror") {
+			case "1":
+				tp := &terrorsproto.Error{}
+				if err := json.Unmarshal(b, tp); err != nil {
+					log.Warn(rsp.ctx, "Failed to unmarshal terror: %v", err)
+					rsp.Error = errors.New(string(b))
+				} else {
+					rsp.Error = terrors.Unmarshal(tp)
+				}
+
+			default:
 				rsp.Error = errors.New(string(b))
-			} else {
-				rsp.Error = terrors.Unmarshal(tp)
 			}
-		} else if rsp.StatusCode >= 500 && rsp.StatusCode <= 599 {
-			b, _ := rsp.BodyBytes(false)
-			rsp.Error = errors.New(string(b))
 		}
 	}
 
