542726fa1c7292289f90e09175bd12ecb707fbd2
Oliver Beattie <oliver@obeattie.com>
2018-09-07 21:03:46 -0700

    Test more edge cases around reading response bodies

--- response_test.go
+++ response_test.go
@@ -5,14 +5,16 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestResponseWriter(t *testing.T) {
 	t.Parallel()
-	req := NewRequest(nil, "GET", "/abc", nil)
+	req := Request{}
 
 	// Using NewResponse, vanilla
 	r := NewResponse(req)
@@ -40,8 +42,7 @@ func TestResponseWriter(t *testing.T) {
 
 func TestResponseWriter_Error(t *testing.T) {
 	t.Parallel()
-	req := NewRequest(nil, "GET", "/", nil)
-	rsp := NewResponse(req)
+	rsp := NewResponse(Request{})
 	rsp.Writer().WriteError(errors.New("abc"))
 	assert.Error(t, rsp.Error)
 	assert.Equal(t, "abc", rsp.Error.Error())
@@ -50,17 +51,125 @@ func TestResponseWriter_Error(t *testing.T) {
 // TestResponseDecodeCloses verifies that a response body is closed after calling Decode()
 func TestResponseDecodeCloses(t *testing.T) {
 	t.Parallel()
-	req := NewRequest(nil, "GET", "/", nil)
-	rsp := NewResponse(req)
-	b := []byte("{\"a\":\"b\"}\n")
+	rsp := NewResponse(Request{})
+	b := []byte(`{"a":"b"}` + "\n")
 	r := newDoneReader(ioutil.NopCloser(bytes.NewReader(b)), -1)
 	rsp.Body = r
 
 	bout := map[string]string{}
-	rsp.Decode(&bout)
+	assert.NoError(t, rsp.Decode(&bout))
+	assert.Equal(t, map[string]string{
+		"a": "b"}, bout)
 	select {
 	case <-r.closed:
 	default:
 		assert.Fail(t, "response body was not closed after Decode()")
 	}
+}
+
+// TestResponseDecodeJSON_TrailingSpace verifies that trailing newlines do not result in a decoding error
+func TestResponseDecodeJSON_TrailingSpace(t *testing.T) {
+	t.Parallel()
+	rsp := NewResponse(Request{})
+	rsp.Body = ioutil.NopCloser(strings.NewReader(`{"foo":"bar"}` + "\n\n\n\n"))
+
+	bout := map[string]string{}
+	assert.NoError(t, rsp.Decode(&bout))
+	assert.Equal(t, map[string]string{
+		"foo": "bar"}, bout)
+}
+
+// rc is a helper type used in tests involving a generic io.ReadCloser
+type rc struct {
+	strings.Reader
+	closed int
+}
+
+func (v *rc) Close() error {
+	v.closed += 1
+	return nil
+}
+
+// TestResponseBodyBytes_Consuming verifies that Response.BodyBytes behaves as expected in consuming mode (ie. where it
+// is expected that future calls to BodyBytes() will return EOF).
+//
+// The BodyBytes function is specialised for efficiency on some common types that Typhon uses as a Response.Body; this
+// test verifies that these specialisations work as expected along with the general io.ReadCloser case.
+func TestResponseBodyBytes_Consuming(t *testing.T) {
+	t.Parallel()
+
+	// Most general case: an opaque io.ReadCloser
+	body := &rc{*strings.NewReader("abc"), 0}
+	rsp := NewResponse(Request{})
+	rsp.Body = body
+	b, err := rsp.BodyBytes(true)
+	require.NoError(t, err)
+	assert.Equal(t, []byte("abc"), b)
+	assert.Equal(t, 1, body.closed) // The reader should have been closed
+
+	// Specialised case: *bufCloser
+	rsp.Body = &bufCloser{*bytes.NewBuffer([]byte("def"))}
+	b, err = rsp.BodyBytes(true)
+	require.NoError(t, err)
+	assert.Equal(t, []byte("def"), b)
+}
+
+// TestResponseBodyBytes_Preserving verifies that Response.BodyBytes behaves as expected in consuming mode (ie. where it
+// is expected that future calls to BodyBytes() will return EOF).
+//
+// The BodyBytes function is specialised for efficiency on some common types that Typhon uses as a Response.Body; this
+// test verifies that these specialisations work as expected along with the general io.ReadCloser case.
+func TestResponseBodyBytes_Preserving(t *testing.T) {
+	t.Parallel()
+
+	// Most general case: an opaque io.ReadCloser
+	body := &rc{*strings.NewReader("abc"), 0}
+	rsp := NewResponse(Request{})
+	rsp.Body = body
+	for i := 0; i < 10; i++ { // Repeated reads should yield the same result
+		b, err := rsp.BodyBytes(false)
+		require.NoError(t, err)
+		assert.Equal(t, []byte("abc"), b)
+		assert.Equal(t, 1, body.closed) // The underlying reader should have been closed exactly once
+	}
+
+	// Specialised case: *bufCloser
+	rsp.Body = &bufCloser{*bytes.NewBuffer([]byte("def"))}
+	for i := 0; i < 100; i++ { // Repeated reads should yield the same result
+		b, err := rsp.BodyBytes(false)
+		require.NoError(t, err)
+		assert.Equal(t, []byte("def"), b)
+	}
+}
+
+func BenchmarkResponseDecode(b *testing.B) {
+	rsp := NewResponse(NewRequest(nil, "GET", "/", nil))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsp.Body = &rc{*strings.NewReader(`{"foo":"bar"}` + "\n"), 0}
+		v := map[string]string{}
+		rsp.Decode(&v)
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkRepeatedResponseDecode(b *testing.B) {
+	rsp := NewResponse(NewRequest(nil, "GET", "/", nil))
+	rsp.Body = &rc{*strings.NewReader(`{"foo":"bar"}` + "\n"), 0}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v := map[string]string{}
+		rsp.Decode(&v)
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkRepeatedResponseBodyBytes(b *testing.B) {
+	rsp := NewResponse(NewRequest(nil, "GET", "/", nil))
+	rsp.Body = &rc{*strings.NewReader(`{"foo":"bar"}` + "\n"), 0}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsp.BodyBytes(false)
+	}
+	b.ReportAllocs()
 }
