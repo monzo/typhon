c1b7b014e516e1036c3afdabbb5a23f80eb2fc96
Oliver Beattie <oliver@obeattie.com>
2018-10-27 10:46:58 +0100

    Add simple example of how to use Typhon 🔢

---
+++ examples/simple.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/monzo/typhon"
+)
+
+func ping(req typhon.Request) typhon.Response {
+	return req.Response("pong")
+}
+
+func main() {
+	router := typhon.NewRouter()
+	router.GET("/ping", ping)
+
+	svc := router.Serve().
+		Filter(typhon.ErrorFilter).
+		Filter(typhon.H2cFilter)
+	srv, err := typhon.Listen(svc, ":8000")
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("👋  Listening on %v", srv.Listener().Addr())
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	log.Printf("☠️  Shutting down")
+	c, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	srv.Stop(c)
+}
