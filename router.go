d95b87f15138eefbdd180f8c67ad08a23c25b7e0
Daniel Chatfield <chatfielddaniel@gmail.com>
2019-05-15 12:34:58 +0100

    Use custom type for router context key

--- router.go
+++ router.go
@@ -9,8 +9,13 @@ import (
 	"github.com/monzo/terrors"
 )
 
+// We use a custom type to guarantee we won't get a collision with another package. Using an anonymous struct type
+// directly means we'd get a collision with any other package that does the same.
+// https://play.golang.org/p/MxhRiL37R-9
+type routerContextKeyType struct{}
+
 var (
-	routerContextKey   = struct{}{}
+	routerContextKey   = routerContextKeyType{}
 	routerComponentsRe = regexp.MustCompile(`(?:^|/)(\*\w*|:\w+)`)
 )
 
