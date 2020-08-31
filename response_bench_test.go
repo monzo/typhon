7bf59e9f3560df813959249caa1a1790131d9eb0
Suhail Patel <me@suhailpatel.com>
2020-08-31 16:57:32 +0100

    Add benchmark test for decoding Protobuf and E2E Protobuf test

--- response_bench_test.go
+++ response_bench_test.go
@@ -1,8 +1,14 @@
 package typhon
 
 import (
+	"bytes"
+	"io/ioutil"
 	"strings"
 	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/monzo/typhon/prototest"
 )
 
 func BenchmarkResponseDecode(b *testing.B) {
@@ -16,6 +22,22 @@ func BenchmarkResponseDecode(b *testing.B) {
 	}
 }
 
+func BenchmarkResponseProtobufDecode(b *testing.B) {
+	b.ReportAllocs()
+
+	g := &prototest.Greeting{Message: "Hello world!", Priority: 1}
+	out, _ := proto.Marshal(g)
+	rsp := NewResponse(NewRequest(nil, "GET", "/", nil))
+	rsp.Header.Set("Content-Type", "application/protobuf")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsp.Body = ioutil.NopCloser(bytes.NewReader(out))
+		g := &prototest.Greeting{}
+		rsp.Decode(g)
+	}
+}
+
 func BenchmarkRepeatedResponseDecode(b *testing.B) {
 	b.ReportAllocs()
 	rsp := NewResponse(NewRequest(nil, "GET", "/", nil))
@@ -24,6 +46,22 @@ func BenchmarkRepeatedResponseDecode(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := map[string]string{}
 		rsp.Decode(&v)
+	}
+}
+
+func BenchmarkRepeatedProtobufDecode(b *testing.B) {
+	b.ReportAllocs()
+
+	g := &prototest.Greeting{Message: "Hello world!", Priority: 1}
+	out, _ := proto.Marshal(g)
+	rsp := NewResponse(NewRequest(nil, "GET", "/", nil))
+	rsp.Header.Set("Content-Type", "application/protobuf")
+	rsp.Body = &rc{*strings.NewReader(string(out)), 0}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		gout := &prototest.Greeting{}
+		rsp.Decode(gout)
 	}
 }
 
