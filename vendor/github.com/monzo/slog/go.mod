822b61027043aa216af919a0ef7513c576fc502a
Lucy Sweet <lucysweet@monzo.com>
2021-11-03 11:11:59 +0000

    Upgrade slog

---
+++ vendor/github.com/monzo/slog/go.mod
@@ -0,0 +1,8 @@
+module github.com/monzo/slog
+
+go 1.13
+
+require (
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+	github.com/stretchr/testify v1.4.0
+)
--- vendor/github.com/monzo/slog/logger.go
+++ vendor/github.com/monzo/slog/logger.go
@@ -1,25 +1,61 @@
 package slog
 
+import "context"
+
 // A Logger is a way of outputting events.
 type Logger interface {
 	Log(evs ...Event)
 	Flush() error
 }
 
-// A MultiLogger sends invocations to multiple Loggers.
-type MultiLogger []Logger
+// LeveledLogger is a logger which logs at different levels.
+type LeveledLogger interface {
+	Critical(ctx context.Context, msg string, params ...interface{})
+	Error(ctx context.Context, msg string, params ...interface{})
+	Warn(ctx context.Context, msg string, params ...interface{})
+	Info(ctx context.Context, msg string, params ...interface{})
+	Debug(ctx context.Context, msg string, params ...interface{})
+	Trace(ctx context.Context, msg string, params ...interface{})
+}
 
-func (ls MultiLogger) Log(evs ...Event) {
-	for _, l := range ls {
-		l.Log(evs...)
+// SeverityLogger is a logger which can log at different severity levels.
+type SeverityLogger struct {
+	Logger
+}
+
+// NewSeverityLogger creates a SeverityLogger which wraps the default logger.
+func NewSeverityLogger() SeverityLogger {
+	return SeverityLogger{
+		Logger: DefaultLogger(),
 	}
 }
 
-func (ls MultiLogger) Flush() error {
-	for _, l := range ls {
-		if err := l.Flush(); err != nil {
-			return err
-		}
-	}
-	return nil
+// Critical writes a Critical event to the logger.
+func (s SeverityLogger) Critical(ctx context.Context, msg string, params ...interface{}) {
+	s.Log(Eventf(CriticalSeverity, ctx, msg, params...))
+}
+
+// Error writes a Error event to the logger.
+func (s SeverityLogger) Error(ctx context.Context, msg string, params ...interface{}) {
+	s.Log(Eventf(ErrorSeverity, ctx, msg, params...))
+}
+
+// Warn writes a Warn event to the logger.
+func (s SeverityLogger) Warn(ctx context.Context, msg string, params ...interface{}) {
+	s.Log(Eventf(WarnSeverity, ctx, msg, params...))
+}
+
+// Info writes a Info event to the logger.
+func (s SeverityLogger) Info(ctx context.Context, msg string, params ...interface{}) {
+	s.Log(Eventf(InfoSeverity, ctx, msg, params...))
+}
+
+// Debug writes a Debug event to the logger.
+func (s SeverityLogger) Debug(ctx context.Context, msg string, params ...interface{}) {
+	s.Log(Eventf(DebugSeverity, ctx, msg, params...))
+}
+
+// Trace writes a Trace event to the logger.
+func (s SeverityLogger) Trace(ctx context.Context, msg string, params ...interface{}) {
+	s.Log(Eventf(TraceSeverity, ctx, msg, params...))
 }
